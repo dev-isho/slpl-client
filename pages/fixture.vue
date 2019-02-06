47dcf604436fe158bc1e6c47861db90bcb97e42e
Isho <ishmaelkargbo5@gmail.com>
2019-02-06 22:01:07 +0000

    fixture

--- pages/fixture.vue
+++ pages/fixture.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="m-5 sm:mx-5 md:mx-10 lg:mx-16 lg:px-10 lg:my-10 py-2 px-2 rounded shadow-md bg-white">
-    <p class="mb-10 ml-2 font-semibold">Fixtures</p>
+    <p class="mb-2 ml-2 md:mb-10">Fixtures</p>
     <div class="" v-for="(match, index) in Fixtures" :key="index">
       <p
-        class="font-semibold font-semibold pb-2 border-b text-sm pl-2"
+        class="pb-2 border-b text-xs pl-2"
       >{{new Date(match.date).toDateString()}}</p>
       <div>
         <div v-for="(game, index) in match.games" :key="index">
           <div class="flex flex-wrap border-b pb-2 mb-10 items-center border-b">
             <div class="w-full md:w-2/3 flex items-center justify-center mt-4">
               <div
-                class="w-2/3 mx-2 text-right text-xs font-semibold flex justify-end items-center"
+                class="w-2/3 mx-2 text-right text-xs flex justify-end items-center"
               >
                 <span class="mr-2">{{game.home.name}}</span>
                 <img
@@ -22,7 +22,7 @@
               <div
                 class="bg-main text-white py-1 px-2 mx-2 text-center text-xs flex flex-wrap font-semibold"
               >VS</div>
-              <div class="w-2/3 mx-2 text-sm font-semibold flex items-center">
+              <div class="w-2/3 mx-2 text-sm flex items-center">
                 <img
                   class="w-8 h-8"
                   :src="`http://192.168.8.108:4000/image/${game.away.image}`"
@@ -34,7 +34,7 @@
             <div class="w-full md:w-1/3 flex justify-center md:justify-start">
               <div class="p-2 text-sm flex items-center">
                 <img src="../assets/stadium.png" class="h-6 w-6 -mt-2">
-                <p class="text-center ml-2 text-xs font-semibold">{{game.home.stadium}}</p>
+                <p class="text-center ml-2 text-xs">{{game.home.stadium}}</p>
               </div>
             </div>
           </div>
