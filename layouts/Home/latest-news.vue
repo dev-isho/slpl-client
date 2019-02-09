2a68e5d19f473b9eff090db0bf0056b1fb9563f5
Isho <ishmaelkargbo5@gmail.com>
2019-02-09 00:40:57 +0000

    news

--- layouts/Home/latest-news.vue
+++ layouts/Home/latest-news.vue
@@ -7,7 +7,7 @@
           class="w-1/2 lg:w-1/3 sm:w-1/2 md:w-1/2 sm:py-0 md:py-2 py-2 flex flex-col"
           v-for="(item, index) in News"
           :key="index"
-          @click="viewNews(news._id)"
+          @click="viewNews(item._id)"
         >
           <div class="bg-white shadow-md rounded overflow-hidden flex flex-1 flex-col mx-1">
             <img
