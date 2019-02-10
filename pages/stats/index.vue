aa180af778b1928f15157e0b6e0f200f60fcc164
Isho <ishmaelkargbo5@gmail.com>
2019-02-10 13:41:27 +0000

    stats

--- pages/stats/index.vue
+++ pages/stats/index.vue
@@ -4,7 +4,7 @@
       <div class="lg:mx-16 lg:px-10 pt-5 mx-10">
         <p class="text-2xl py-5">Stats Centre</p>
         <div class="flex mt-5">
-          <div class="w-1/5 bg-green rounded-t overflow-hidden mx-1">
+          <div class="w-1/3 md:w-1/5 bg-green rounded-t overflow-hidden mx-1">
             <router-link
               :to="`/stats/dashboard`"
               class="block no-underline text-center text-xs py-2 md:text-sm text-main font-semibold"
@@ -12,14 +12,14 @@
               exact
             >Dashboard</router-link>
           </div>
-          <div class="w-1/5 bg-green rounded-t overflow-hidden mx-1">
+          <div class="w-1/3 md:w-1/5 bg-green rounded-t overflow-hidden mx-1">
             <router-link
               :to="`/stats/players`"
               class="block no-underline text-center text-xs py-2 md:text-sm text-main font-semibold"
               active-class="bg-white text-main"
             >Player Stats</router-link>
           </div>
-          <div class="w-1/5 bg-green rounded-t overflow-hidden mx-1">
+          <div class="w-1/3 md:w-1/5 bg-green rounded-t overflow-hidden mx-1">
             <router-link
               :to="`/stats/clubs`"
               class="block no-underline text-center text-xs py-2 md:text-sm text-main font-semibold"
